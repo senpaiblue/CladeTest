4df5da20cb7e34cd47530f3e47c7e1c13ad9d9d9
senpaiblue <singhsakcham29@email.com>
2024-06-28 23:38:21 +0530

    bug-fix

--- components/HeaderLables.tsx
+++ components/HeaderLables.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeaderLables = ({icon,text}:{icon:any,text:string}) => {
   return (
     <div>
-        <h2 className="gap-2 flex flex-row text-[#5D5D5D] text-[20px] font-medium">{icon} {text}</h2>
+        <h2 className="gap-2 flex flex-row text-[#5D5D5D] items-center text-[20px] font-medium">{icon} {text}</h2>
     </div>
   )
 }
